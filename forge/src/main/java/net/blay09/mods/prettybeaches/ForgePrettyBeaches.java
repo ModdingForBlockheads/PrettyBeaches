fe2f1fc7752c081e3b1f8ccc4b9b5a615f0e5aec
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-13 20:52:02 +0100

    feat: Update for Forge 1.21.4

--- forge/src/main/java/net/blay09/mods/prettybeaches/ForgePrettyBeaches.java
+++ forge/src/main/java/net/blay09/mods/prettybeaches/ForgePrettyBeaches.java
@@ -1,14 +1,16 @@
 package net.blay09.mods.prettybeaches;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.EmptyLoadContext;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(PrettyBeaches.MOD_ID)
 public class ForgePrettyBeaches {
 
-    public ForgePrettyBeaches() {
-        Balm.initialize(PrettyBeaches.MOD_ID, EmptyLoadContext.INSTANCE, PrettyBeaches::initialize);
+    public ForgePrettyBeaches(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
+        Balm.initialize(PrettyBeaches.MOD_ID, loadContext, PrettyBeaches::initialize);
     }
 
 }
